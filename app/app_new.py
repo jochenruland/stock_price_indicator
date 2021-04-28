10281e4e2b2f1a16bfe8c848d2e216252e616fb1
jochenruland <jochenhgruland@gmail.com>
2021-04-28 11:07:38 +0200

    BUGFIXING: connection between frontend and web server

--- app/app_new.py
+++ app/app_new.py
@@ -1,29 +1,14 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, jsonify
-
-import sqlite3
 from werkzeug.exceptions import abort
 
-# Aus Vorlage WebApp ML Pipeline
 import json
-import plotly
 import pandas as pd
 
-from plotly.graph_objs import Bar
+from data_wrangling.process_data import StockDataAnalysis
+from data_wrangling.train_classifier import ModelStockPrice
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
+# import graph objects as "go"
+import plotly.graph_objs as go
 
 
 app = Flask(__name__)
@@ -31,56 +16,70 @@ app.config['SECRET_KEY'] = '928272625242322212'
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
 
     if request.method == 'POST':
         ticker = request.form['ticker']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
 
         if not ticker:
             flash('Ticker symbol is required')
+        elif not start_date:
+            flash('Start date is required')
+        elif not end_date:
+            flash('End date is required')
         else:
-            conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?,?)',
-                         (ticker, 'test content'))
-            conn.commit()
-            posts = conn.execute('SELECT * FROM posts').fetchall()
-            conn.close()
-            return redirect(url_for('post'), posts=posts)
+            return redirect(url_for('post'))
 
-    return render_template('index.html', posts=posts)
+    return render_template('index.html')
 
 @app.route('/post')
 def post():
+    return render_template('post.html')
+
+
+'''
     post = get_post(post_id)
 
     # extract data needed for visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
 
-    # create visuals
+    st_data = StockDataAnalysis([ticker], start_date, end_date, pred_days=7)
+    st_data.setup_features()
+    df_indicators = st_data.create_indicator_dataframe()
+    st_data.create_train_test_data(symbol=ticker, train_size=0.8)
 
-    graphs = [
-        {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
-                )
-            ],
+    st_model = ModelStockPrice()
+    st_model.fit(st_data)
+    print(st_model.predict(st_data))
 
-            'layout': {
-                'title': 'Distribution of Message Genres',
-                'yaxis': {
-                    'title': "Count"
-                },
-                'xaxis': {
-                    'title': "Genre"
-                }
-            }
-        }
-    ]
+
+#-----------------------------------
+    # Creating trace1
+    trace1 = go.Scatter(
+                        x = df.world_rank,
+                        y = df.citations,
+                        mode = "lines",
+                        name = "citations",
+                        marker = dict(color = 'rgba(16, 112, 2, 0.8)'),
+                        text= df.university_name)
+    # Creating trace2
+    trace2 = go.Scatter(
+                        x = df.world_rank,
+                        y = df.teaching,
+                        mode = "lines+markers",
+                        name = "teaching",
+                        marker = dict(color = 'rgba(80, 26, 80, 0.8)'),
+                        text= df.university_name)
+    data = [trace1, trace2]
+    layout = dict(title = 'Citation and Teaching vs World Rank of Top 100 Universities',
+                  xaxis= dict(title= 'World Rank',ticklen= 5,zeroline= False)
+                 )
+    fig = dict(data = data, layout = layout)
+    iplot(fig)
+
+#------------------------------------------------
 
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
@@ -92,6 +91,10 @@ def post():
 
 
     return render_template('post.html', post=post)
+
+
+
+#------------------------------------------------------------------
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
@@ -141,3 +144,4 @@ def delete(id):
     conn.close()
     flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect(url_for('index'))
+'''
