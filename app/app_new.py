a476ff9c6b1c8f1d3cf2238fa213492c277a6d1b
jochenruland <jochenhgruland@gmail.com>
2021-05-12 11:06:00 +0200

    ADD model generation and prediction to def post()

--- app/app_new.py
+++ app/app_new.py
@@ -3,6 +3,7 @@ from werkzeug.exceptions import abort
 
 import json, plotly
 import pandas as pd
+import datetime as dt
 
 from data_wrangling.process_data import StockDataAnalysis
 from data_wrangling.train_classifier import ModelStockPrice
@@ -45,11 +46,14 @@ def post():
     st_data = StockDataAnalysis(ticker, start_date, end_date)
     st_data.setup_features()
     df_indicators = st_data.create_indicator_dataframe()
-    #st_data.create_train_test_data(symbol=ticker, train_size=0.8)
 
-    #st_model = ModelStockPrice()
-    #st_model.fit(st_data)
-    #pred_values = st_model.predict(st_data)
+    start_pred = start_date +
+    st_model = ModelStockPrice(start_predict='2021-04-28', end_predict='2021-05-07')
+    st_model.create_train_test_data(st_data, train_size=0.7)
+    st_model.fit()
+    print(st_model.predict())
+    st_model.evaluate_model_performance()
+
 
     # Creating the plots for the website
     # 1. create a ploty graph_objs for each plot
