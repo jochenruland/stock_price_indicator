e1b3e3c0f1ad0b7c61b978f1258c3c2d452100d7
jochenruland <jochenhgruland@gmail.com>
2021-05-16 08:10:45 +0200

    EDIT main() transferred into seperate file application.py

--- app/data_wrangling/train_classifier.py
+++ app/data_wrangling/train_classifier.py
@@ -137,22 +137,3 @@ class ModelStockPrice():
             plt.show()
 
         return result_list
-
-
-
-def main(symbol='AAPL', start_date='2020-11-01', end_date='2021-04-27'):
-    from process_data import StockDataAnalysis
-
-    st_data = StockDataAnalysis(start_date=start_date, end_date=end_date)
-    st_data.setup_features()
-    df_indicators = st_data.create_indicator_dataframe()
-    print(df_indicators.head(50))
-
-    st_model = ModelStockPrice(start_predict='2021-04-28', end_predict='2021-05-07')
-    st_model.create_train_test_data(st_data, train_size=0.7)
-    st_model.fit()
-    print(st_model.predict())
-    st_model.evaluate_model_performance()
-
-if __name__ == '__main__':
-    main()
