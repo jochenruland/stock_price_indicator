cdaaf7330685364e7fe8cfd9a3f4f9e6198baf71
jochenruland <jochenhgruland@gmail.com>
2021-05-17 10:59:39 +0200

    DEL unnecessary code

--- app/ETL.py
+++ app/ETL.py
@@ -33,32 +33,15 @@ def main(symbol='AAPL'):
     if not symbol_lst:
         return print("No ticker symbol was entered")
     else:
-        data_lst=[]
         for s in symbol_lst:
             s=s.upper()
             st_data = StockDataAnalysis(symbol=s,start_date=sd, end_date=ed)
             st_data.setup_features()
             df_indicators = st_data.create_indicator_dataframe()
-            data_lst.append(dict(symbol = s, data = df_indicators ))
             conn = sqlite3.connect('indicators.db')
             df_indicators.to_sql('s', con = conn, if_exists='replace', index=False)
 
-        print(data_lst)
-
-
-    #print('Enter a comma seperated list of ticker symbols, \n you want to make predictions for (all or subset of previous list):')
-    #pred_str = input()
-    #pred_lst = pred_str.strip(' ').split(",")
-
-    #print('Enter a start date for predictions:')
-
-
-    #st_model = ModelStockPrice(start_predict='2021-04-28', end_predict='2021-05-07')
-    #st_model.create_train_test_data(st_data, train_size=0.7)
-    #st_model.fit()
-    #print(st_model.predict())
-    #result = st_model.evaluate_model_performance()
-    #print(result)
+        print('Stock data has been saved to indicators.db')
 
 if __name__ == '__main__':
     main()
