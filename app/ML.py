8da23ce885273ad5706b5eb1ea1917a477721a8c
jochenruland <jochenhgruland@gmail.com>
2021-05-18 11:56:33 +0200

    ADD exception handling

--- app/ML.py
+++ app/ML.py
@@ -23,8 +23,8 @@ def main(symbol='AAPL'):
     elif (ed - sd).days > 7:
         return print('No predictions for more than 7 days will be made')
 
-    #elif sd > dt.datetime.now():
-        #return print('End date must be equal or before actual date')
+    elif sd > dt.datetime.now():
+        return print('Start of prediction must be equal or before actual date')
     else:
         print('Enter a comma seperated list of symbols to make predticions for. \
             \n Data must have been selected via ETY.py previously:')
@@ -36,26 +36,32 @@ def main(symbol='AAPL'):
     else:
         conn = sqlite3.connect('indicators.db')
         for s in symbol_lst:
-            s=s.upper()
-            indicators_df = pd.read_sql('SELECT * FROM {}'.format(s), conn)
+            try:
+                s=s.upper()
+                indicators_df = pd.read_sql('SELECT * FROM {}'.format(s), conn)
 
-            st_model = ModelStockPrice(start_predict=sd, end_predict=ed)
+                st_model = ModelStockPrice(start_predict=sd, end_predict=ed)
 
-            st_model.create_train_test_data(indicators_df, train_size=0.7)
-            st_model.fit()
+                st_model.create_train_test_data(indicators_df, train_size=0.7)
+                st_model.fit()
 
-            predict, future = st_model.predict()
-            print('\n','Symbol:{}'.format(s),'\n')
+                predict, future = st_model.predict()
+                print('\n','Symbol:{}'.format(s),'\n')
 
-            print('Trad.Day  Price\n-----------------------------------')
-            for i in range(len(future)):
-                print("Day {} :   {}".format(i, future[i]))
+                print('Trad.Day  Price\n-----------------------------------')
+                for i in range(len(future)):
+                    print("Day {} :   {}".format(i, future[i]))
 
-            result = st_model.evaluate_model_performance()
-            print('\n','Evaluation of underlying model\n-----------------------------------')
-            for r in result:
-                print(r['indicator'],':   ', r['val'])
-            print('\n')
+                result = st_model.evaluate_model_performance()
+                print('\n','Evaluation of underlying model\n-----------------------------------')
+                for r in result:
+                    print(r['indicator'],':   ', r['val'])
+                print('\n')
+            except Exception as e:
+                print('Execption occurred: {}'.format(e))
+
+        return
+
 
 if __name__ == '__main__':
     main()
